7f32b19817c5ca9512a2dce8cb4ca05f15651a20
RaphaelBecker <raphael.becker.private@outlook.de>
2022-11-10 15:53:40 +0100

    slowed logging down to save storage

--- mobile_extension/system_status.py
+++ mobile_extension/system_status.py
@@ -19,8 +19,6 @@ class SystemStatus(Thread):
         self.number_of_cpus = psutil.cpu_count()
         self.obj_disk = psutil.disk_usage('/')
         self.total_disc = self.obj_disk.total / (1024.0 ** 3)  # GB (^3)
-        self.used_disc = self.obj_disk.used / (1024.0 ** 3)  # GB (^3)
-        self.free_disc = self.obj_disk.free / (1024.0 ** 3)  # GB (^3)
         self.disc_in_use = self.obj_disk.percent  # %
         # Sniffer stats:
         self.usb = usb
@@ -35,11 +33,11 @@ class SystemStatus(Thread):
 
     def run(self):
         while True:
-            logger.info(self.get_system_stats_row())
+            self.update_sniffer_stats()
             self.update_system_health_stats()
-            for i in range(1, 3, 1):
-                self.update_sniffer_stats()
-                time.sleep(.8)
+            logger.info(self.get_system_stats_row()[0])
+            logger.info(self.get_system_stats_row()[1])
+            time.sleep(3)
 
     def update_system_health_stats(self):
         self.ram_percent = psutil.virtual_memory().percent  # %
@@ -49,8 +47,6 @@ class SystemStatus(Thread):
         self.cpu_usage = psutil.cpu_percent()  # %
         self.number_of_cpus = psutil.cpu_count()
         self.total_disc = self.obj_disk.total / (1024.0 ** 3)  # GB
-        self.used_disc = self.obj_disk.used / (1024.0 ** 3)  # GB
-        self.free_disc = self.obj_disk.free / (1024.0 ** 3)  # GB
         self.disc_in_use = self.obj_disk.percent  # %
 
     def update_sniffer_stats(self):
@@ -59,24 +55,23 @@ class SystemStatus(Thread):
         self.sst_button_state = self.sst_tracing_button.get_button_state()
         self.led = self.status_led.colour
 
-    def get_system_stats_row(self) -> str:
-        stats_list = ["OS_status:  ram: {:.2f} %".format(self.ram_percent),
+    def get_system_stats_row(self) -> []:
+        stats_list = ["OS_status:       ram: {:.2f} %".format(self.ram_percent),
                       " total_memory: {:.2f} MB".format(self.total_memory),
                       " available_memory: {:.2f} MB".format(self.available_memory),
                       " used_memory: {:.2f} MB".format(self.used_memory),
                       " cpu_usage: {:.2f}".format(self.cpu_usage),
                       " number_of_cpus: {:.2f}".format(self.number_of_cpus),
                       " total_disc: {:.2f} GB".format(self.total_disc),
-                      " used_disc: {:.2f} GB".format(self.used_disc),
-                      " free_disc: {:.2f} GB".format(self.free_disc),
-                      " disc_in_use: {:.2f} GB".format(self.disc_in_use),
-                      "  —  Sniffer_status:  usb_mounted: " + str(self.usb_mounted),
-                      " state: " + self.state,
-                      " sst_button_state: " + str(self.sst_button_state),
-                      " led: " + str(self.led)
+                      " disc_in_use: {:.2f} %".format(self.disc_in_use),
                       ]
+        sniffer_stats = ["Sniffer_status:  usb_mounted: " + str(self.usb_mounted),
+                        " state: " + self.state,
+                        " sst_button_state: " + str(self.sst_button_state),
+                        " led: " + str(self.led)
+                        ]
 
-        return ",".join(stats_list)
+        return [",".join(stats_list), ",".join(sniffer_stats)]
 
     def print_system_stats(self):
         print()
@@ -96,6 +91,4 @@ class SystemStatus(Thread):
         # change to psutil.disk_usage('C:') on windows.
         obj_disk = psutil.disk_usage('/')
         print("total Disk  :", self.total_disc, 'GB')
-        print("used Disk  :", self.used_disc, 'GB')
-        print("free Disk  :", self.free_disc, 'GB')
         print(self.disc_in_use, "% in use")
